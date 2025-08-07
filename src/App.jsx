82ce7be3c3928d4d62df2644df16555e7693e59d
kambhampatinarendra123 <kambhampatinarendra123@gmail.com>
2025-08-07 12:23:51 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S219_CICD">
+    <Router basename="/SEC116/">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
